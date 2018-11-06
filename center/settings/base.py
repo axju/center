8d44cf0b08876f234d2285e92bda2a3f3cd32bb4
axju <axel.juraske@short-report.de>
2018-11-06 07:23:07 +0100

    change settings

--- center/settings/base.py
+++ center/settings/base.py
@@ -67,7 +67,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
-    'deploying',
 
     'alpha',
     'core',
--- center/settings/production.py
+++ center/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['mysite.com', ]
 
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.mailgun.org'
 #EMAIL_PORT = 587
 #EMAIL_HOST_USER = config('EMAIL_HOST_USER')
